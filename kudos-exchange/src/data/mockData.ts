6cb86f523e32479f9921aeaf0b3000878e8a4bda
Jawadkhera <me@example.com>
2026-01-10 23:24:05 +0500

    changed name of sarah chen

--- kudos-exchange/src/data/mockData.ts
+++ kudos-exchange/src/data/mockData.ts
@@ -47,7 +47,7 @@ export const USERS: UserType[] = [
     CURRENT_USER,
     {
         id: 'u2',
-        name: 'Sarah Chen',
+        name: 'Laiba Ahmad',
         avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         jobTitle: 'Frontend Dev',
     },
